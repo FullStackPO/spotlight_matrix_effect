6703d5baaa4c8dd48d5b68ca0164e50212fc07da
Harsh Arya <harsharya8946@gmail.com>
2026-03-01 00:01:22 +0530

    complete matrix effect

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ let matrix = document.querySelector(".matrix");
 let h1 = document.querySelector(".matrix h1");
 let text = h1.innerText;
 let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let iteration = 0
 
 matrix.addEventListener("mouseenter", function(){
-    setInterval(()=>{
-    const str = text.split('').map(()=>{
-        return character.split('')[Math.floor(Math.random()*53)]
+
+    setInterval(() => {
+    const str = text.split('').map((char,index) => {
+    if(index < iteration){
+        return char
+    }
+    return character.split('')[Math.floor(Math.random() * 53)]
     }).join('')
     h1.innerText = str
-    },100)
+
+    iteration += 0.15
+    },50)
 })
