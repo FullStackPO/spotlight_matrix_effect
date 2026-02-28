75df199a2fa96ce4132919fa09e01906c4772af1
Harsh Arya <harsharya8946@gmail.com>
2026-02-28 18:38:20 +0530

    start with matrix effect

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ addEventListener("mousemove", function(e){
     document.body.style.setProperty("--x", e.clientX + "px");
     document.body.style.setProperty("--y", e.clientY + "px");
 })
+
+let matrix = document.querySelector(".matrix");
+let h1 = document.querySelector(".matrix h1");
+let text = h1.innerText;
+let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+matrix.addEventListener("mouseenter", function(){
+    setInterval(()=>{
+    const str = text.split('').map(()=>{
+        return character.split('')[Math.floor(Math.random()*53)]
+    }).join('')
+    h1.innerText = str
+    },100)
+})
